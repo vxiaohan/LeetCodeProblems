f1b8580668e2db43d85a39871d6779f75a051bef
vxiaohan <xhdtc@126.com>
2017-11-29 10:11:27 +0800

    Finish 714 Best Time to Buy and Sell Stock with Transaction Fee

--- 714_BestTimetoBuyandSellStockwithTransactionFee/714_BestTimetoBuyandSellStockwithTransactionFee.py
+++ 714_BestTimetoBuyandSellStockwithTransactionFee/714_BestTimetoBuyandSellStockwithTransactionFee.py
@@ -10,19 +10,13 @@ class Solution:
         """
         if len(prices) < 2:
             return 0
+        sellProfit = [0] * len(prices)
         holdProfit = [0] * len(prices)
-        unholdProfit = [0] * len(prices)
-        buyProfit = [0] * len(prices)
-        sellProfit = [0] *len(prices)
-        holdProfit[0] = -2
-        buyProfit[0] = -2
+        holdProfit[0] = - prices[0]
         for i in range(1, len(prices)):
-            holdProfit[i] = max([holdProfit[i - 1] + (prices[i] - prices[i - 1]), unholdProfit[i - 1] - fee])
-            unholdProfit[i] = max(unholdProfit[i-1], holdProfit[i-1])
-        holdProfit[-1] -= fee
-        print(holdProfit)
-        print(unholdProfit)
-        return max([holdProfit[-1], unholdProfit[-1]])
+            sellProfit[i] = max([sellProfit[i - 1], holdProfit[i - 1] + prices[i] - fee])
+            holdProfit[i] = max([holdProfit[i - 1], sellProfit[i - 1] - prices[i]])
+        return sellProfit[-1]
 
 
 class BestTimeTestCase(unittest.TestCase):
@@ -30,6 +24,8 @@ class BestTimeTestCase(unittest.TestCase):
         test = Solution()
         self.assertEqual(test.maxProfit([1, 3, 2, 8, 4, 9], 2), 8)
         self.assertEqual(test.maxProfit([1, 5, 9], 2), 6)
+        self.assertEqual(test.maxProfit([1, 3, 7, 5, 10, 3], 3), 6)
+        self.assertEqual(test.maxProfit([1, 4, 6, 2, 8, 3, 10, 14], 3), 13)
 
 
 if __name__ == '__main__':

34b148161d45cae1837e54c9872f057a42147b70
vxiaohan <xhdtc@126.com>
2017-12-06 19:26:20 +0800

    Work with 120

---
+++ 120_Triangle/120_Triangle.py
@@ -0,0 +1,16 @@
+import unittest
+
+class Solution:
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+
+class TestMinimumTotal(unittest.TestCase):
+    def testMinimumTotal(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

c686edd090a3ef346ba9c0c2d997091b176bf2bc
vxiaohan <xhdtc@126.com>
2017-12-22 14:04:55 +0800

    Optimize 650

--- 650_ 2KeysKeyboard/650_ 2KeysKeyboard.py
+++ 650_ 2KeysKeyboard/650_ 2KeysKeyboard.py
@@ -27,7 +27,7 @@ class Solution:
         return dp[-1][-1]
 '''
 
-
+'''
 class Solution:
     def minSteps(self, n):
         """
@@ -43,14 +43,37 @@ class Solution:
                 if j % i == 0:
                     dp[j] = dp[j] if dp[j] < dp[i] + j / i else dp[i] + j / i
         return int(dp[-1])
+'''
 
+class Solution:
+    def minSteps(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 1:
+            return 0
+        dp = [i for i in range(n + 1)]
+        dp[0] = dp[1] = 0
+        for i in range(2, int(n/2)+1):
+            index = i + i
+            k = 2
+            value = 0
+            value_i = dp[i]
+            while  index <= n:
+                value = dp[index]
+                if value > value_i + k:
+                    dp[index] = value_i + k
+                k+=1
+                index += i
+        return dp[-1]
 
 class TestMinSteps(unittest.TestCase):
     def testMinSteps(self):
         test = Solution()
-        # self.assertEqual(test.minSteps(3), 3)
-        #self.assertEqual(test.minSteps(4), 4)
-        # self.assertEqual(test.minSteps(6), 5)
+        self.assertEqual(test.minSteps(3), 3)
+        self.assertEqual(test.minSteps(4), 4)
+        self.assertEqual(test.minSteps(6), 5)
         self.assertEqual(test.minSteps(908), 231)
 
 
